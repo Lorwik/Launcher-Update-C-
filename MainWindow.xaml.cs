bd182efe2e20eafeb0dba00bf00e8838f1b7c9e5
Lorwik <manueljsandalio@gmail.com>
2020-06-03 15:01:07 +0200

    Controlada excepcion de desempaquetado de zip haciendo las actualizaciones de forma recursiva

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,176 +16,205 @@ using System.Windows.Media;
 
 namespace Uplauncher{
 
-  public partial class MainWindow : Window, IComponentConnector{
+      public partial class MainWindow : Window, IComponentConnector{
 
-    //ATRIBUTOS
-    private string versionActual;
-    private string versionCliente;
-    private int ParcheActual;
+            //ATRIBUTOS
+            private string versionRemota;   //Version actual del cliente remoto nos dice si hay parches
+            private string versionLocal;    //Version actual Local del clienteo
+            private double ParcheActual;    //Indica el parche en el que nos encontramos actualmente
+            private bool UpdatePendiente;   //Bandera que nos indica si tenemos actualizaciones pendientes
+            private bool Actualizando;      //Bandera que nos indica si estamos en proceso de actualizacion
+            private readonly string URLWeb = "http://winterao.com.ar";
+            //METODOS
 
-    //METODOS
+        /**
+         * Main
+         */
+            public MainWindow(){
+                this.InitializeComponent();
 
-    /**
-     * Main
-     */
-    public MainWindow(){
-        this.InitializeComponent();
-        this.btnJugar.IsEnabled = false;
-        this.ParcheActual = 0;
+                //Inicializamos las variables
+                this.ParcheActual = 0;
+                this.Actualizando = false;
+                
+                //Comprobamos la version actual del cliente
+                if (this.VerifyVersion()){
+                    this.UpdatePendiente = false;
+                    this.pbar.Value = 100.0;
+                    this.lblDow.Content = (object)"Actualizado";
+                    this.lblDow.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#00D62D"));
 
-       //Comprobamos la version actual del cliente
-        if (this.VerifyVersion()){
-            this.pbar.Value = 100.0;
-            this.btnJugar.IsEnabled = true;
-            this.lblDow.Content = (object) "Actualizado";
-            this.lblDow.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#00D62D"));
+                }else{ //Si el cliente no esta actualizado, lo notificamos
+                    this.UpdatePendiente = true;
+                    this.lblDow.Content = (object)"NO Actualizado.";
+                    this.lblDow.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF0000"));
+                }
 
-        }else{
-            for(int i = Convert.ToInt32(this.versionCliente) + 1; i <= Convert.ToInt32(this.versionActual); i++) {
-                //Anotamos el parche que estamos descargando actualmente
-                this.ParcheActual = i;
-                this.lblDow.Content = "Descargando actualizacion " + i;
+                //Guardamos el parche actual
+                this.ParcheActual = Convert.ToInt32(this.versionLocal);
 
-               //Comenzamos el proceso de actualizacion
-                string DownloadTo = Directory.GetCurrentDirectory() + "\\update" + i + ".zip";
-                this.lblDow.Content = (object) "Actualizando...";
-                this.Download("http://winterao.com.ar/update/update" + i + ".zip", DownloadTo);
-                lblDow.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#0092D6"));
+        }
 
-               //Extraemos
-               this.MyExtract();
+            /**
+             * Inicia el proceso de actualizacion del cliente
+             */
+            private void Actualizar(){
+                //¿El parche actual coincide con la version remota?
+                if (Convert.ToInt32(this.ParcheActual) < Convert.ToInt32(this.versionRemota)){
+
+                    this.ParcheActual++;
+                    //Anunciamos el parche que estamos descargando
+                    this.lblDow.Content = "Descargando actualizacion " + this.ParcheActual;
+
+                    //Descargamos e instalamos
+                    string DownloadTo = Directory.GetCurrentDirectory() + "\\update" + this.ParcheActual + ".zip";
+                    this.lblDow.Content = (object)"Actualizando...";
+                    this.Download(URLWeb +"/update/update" + this.ParcheActual + ".zip", DownloadTo);
+                    lblDow.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#0092D6"));
+
+                }else{ 
+
+                    //int num1 = (int)MessageBox.Show("¡WinterAO ha sido actualizado! Ahora puedes jugar a la ultima versión.", "Notification");
+                    this.Actualizando = false;
+                    this.lblDow.Content = (object)"Actualizado";
+                    this.lblDow.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#00D62D"));
+                }
             }
 
-            //int num1 = (int)MessageBox.Show("¡WinterAO ha sido actualizado! Ahora puedes jugar a la ultima versión.", "Notification");
-            this.btnJugar.IsEnabled = true;
-            this.lblDow.Content = (object)"Actualizado";
-            this.lblDow.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#00D62D"));
-        }
-    }
+            /**
+             * Comprueba la ultima version disponible
+             */
+            private bool VerifyVersion(){
+                StreamReader streamReader = new StreamReader(Directory.GetCurrentDirectory() + "\\version.txt");
+                this.versionLocal = streamReader.ReadToEnd();
+                this.versionRemota = this.ReadRemoteTextFile(URLWeb + "/update/version.txt");
+                streamReader.Close();
 
-    /**
-     * Comprueba la ultima version disponible
-     */
-    private bool VerifyVersion(){
-        StreamReader streamReader = new StreamReader(Directory.GetCurrentDirectory() + "\\version.txt");
-        this.versionCliente = streamReader.ReadToEnd();
-        this.versionActual = this.ReadRemoteTextFile("http://winterao.com.ar/update/version.txt");
-        streamReader.Close();
+                return this.versionRemota == versionLocal;
+            }
 
-        return this.versionActual == versionCliente;
-    }
+            private string ReadRemoteTextFile(string Url){
+                Stream responseStream = WebRequest.Create(new Uri(Url)).GetResponse().GetResponseStream();
+                StreamReader streamReader = new StreamReader(responseStream);
+                string end = streamReader.ReadToEnd();
+                streamReader.Close();
+                responseStream.Close();
 
-    private string ReadRemoteTextFile(string Url){
-        Stream responseStream = WebRequest.Create(new Uri(Url)).GetResponse().GetResponseStream();
-        StreamReader streamReader = new StreamReader(responseStream);
-        string end = streamReader.ReadToEnd();
-        streamReader.Close();
-        responseStream.Close();
+                return end;
+            }
 
-        return end;
-    }
-
-    /**
-     * Descarga la actualizacion
-     */
-    private void Download(string Url, string DownloadTo){
-         WebClient webClient = new WebClient();
-         webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(this.UpdateProgressChange);
-         webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(this.UpdateDone);
-         webClient.DownloadFileAsync(new Uri(Url), DownloadTo);
-         webClient.Dispose();
-    }
-
-    /**
-     * Extrae el .zip
-     */
-    private void MyExtract(){
-        string str = Directory.GetCurrentDirectory() + "\\update" + ParcheActual + ".zip";
-        string baseDirectory = Directory.GetCurrentDirectory();
-            int num1 = (int)MessageBox.Show("update" + ParcheActual + ".zip");
-            if (!System.IO.File.Exists(str))
-            return;
-        using (ZipFile zipFile = ZipFile.Read(str))
-        {
-            foreach (ZipEntry zipEntry in zipFile)
-            zipEntry.Extract(baseDirectory, ExtractExistingFileAction.OverwriteSilently);
-        }
-        try{
-            System.IO.File.Delete(str);
-        }catch (IOException ex){
-            int num = (int) MessageBox.Show(ex.Message);
-        }
-    }
-
-    /**
-     * Actualiza la barra de progreso
-     */
-    private void UpdateProgressChange(object sender, DownloadProgressChangedEventArgs e){
-        this.pbar.Value = (double) e.ProgressPercentage;
-
-        if (this.pbar.Value != 100.0) { 
-            return;
+            /**
+             * Descarga la actualizacion
+             */
+            private void Download(string Url, string DownloadTo){
+                 WebClient webClient = new WebClient();
+                 webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(this.UpdateProgressChange);
+                 webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(this.UpdateDone);
+                 webClient.DownloadFileAsync(new Uri(Url), DownloadTo);
+                 webClient.Dispose();
         }
 
-       
-    }
+            /**
+             * Extrae el .zip
+             */
+            private void MyExtract(){
+                string str = Directory.GetCurrentDirectory() + "\\update" + ParcheActual + ".zip";
+                string baseDirectory = Directory.GetCurrentDirectory();
 
-    /**
-     * Completa la actualizacion
-     */
-    private void UpdateDone(object sender, AsyncCompletedEventArgs e){
+                if (!System.IO.File.Exists(str))
+                    return;
+                using (ZipFile zipFile = ZipFile.Read(str)){
+                    foreach (ZipEntry zipEntry in zipFile)
+                    zipEntry.Extract(baseDirectory, ExtractExistingFileAction.OverwriteSilently);
+                }
+                try{
+                    System.IO.File.Delete(str);
+                    //Volvemos a llamar al Actualizar
+                    this.Actualizar();
+                }catch (IOException ex){
+                    int num = (int) MessageBox.Show(ex.Message);
+                }
+            }
 
-        //Posible mensaje de parche instalado
+            /**
+             * Actualiza la barra de progreso
+             */
+            private void UpdateProgressChange(object sender, DownloadProgressChangedEventArgs e){
+                this.pbar.Value = (double) e.ProgressPercentage;
 
-        try{
-            System.IO.File.Delete(Directory.GetCurrentDirectory() + "\\version.txt");
-        }catch (IOException ex){
-            int num2 = (int) MessageBox.Show(ex.Message);
-        }
+                if (this.pbar.Value != 100.0) { 
+                    return;
+                }
+            }
 
-        StreamWriter streamWriter = new StreamWriter(Directory.GetCurrentDirectory() + "\\version.txt");
-        streamWriter.Write(this.ParcheActual);
-        streamWriter.Close();
-    }
+            /**
+             * Completa la actualizacion
+             */
+            private void UpdateDone(object sender, AsyncCompletedEventArgs e){
 
-    /**
-     * Boton para ir a la web
-     */
-    private void btnSitio_Click(object sender, RoutedEventArgs e){
-        Process.Start("https://winterao.com.ar/");
-    }
+                //Posible mensaje de parche instalado
+                //Extraemos
+                this.MyExtract();
+            try
+            {
+                    System.IO.File.Delete(Directory.GetCurrentDirectory() + "\\version.txt");
+                }catch (IOException ex){
+                    int num2 = (int) MessageBox.Show(ex.Message);
+                }
 
-    private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e){
-        this.DragMove();
-    }
+                StreamWriter streamWriter = new StreamWriter(Directory.GetCurrentDirectory() + "\\version.txt");
+                streamWriter.Write(this.ParcheActual);
+                streamWriter.Close();
+            }
 
-    /**
-     * Boton Salir
-     */
-    private void btnSalir_Click(object sender, RoutedEventArgs e){
-        this.Close();
-    }
+            /**
+             * Boton para ir a la web
+             */
+            private void btnSitio_Click(object sender, RoutedEventArgs e){
+                Process.Start(URLWeb);
+            }
 
-    /**
-     * Boton de jugar
-     */
-    private void btnJugar_Click(object sender, RoutedEventArgs e){
-        /*if (!System.IO.File.Exists("AH.dll"))
-        {
-        int num1 = (int) MessageBox.Show("No se ha podido encontrar el antihack!", "Error", MessageBoxButton.OK, MessageBoxImage.Hand);
-        }*/
-        if (!System.IO.File.Exists("WinterAO Resurrection.exe")){
-            int num2 = (int) MessageBox.Show("No se ha encontrado el ejecutable de WinterAO Resurrection!", "Error", MessageBoxButton.OK, MessageBoxImage.Hand);
-        }else if (new Process(){
-            StartInfo = new ProcessStartInfo("WinterAO Resurrection.exe")
-        }.Start());
-    }
+            private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e){
+                this.DragMove();
+            }
 
-    /**
-     * Boton de minimizar
-     */
-    private void btnMini_Click(object sender, RoutedEventArgs e){
-        this.WindowState = WindowState.Minimized;
-    }
+            /**
+             * Boton Salir
+             */
+            private void btnSalir_Click(object sender, RoutedEventArgs e){
+                this.Close();
+            }
+
+            /**
+             * Boton de jugar
+             */
+            private void btnJugar_Click(object sender, RoutedEventArgs e){
+                if (this.Actualizando == false){
+                    //¿Hay actualizaciones pendientes?
+                    if (this.UpdatePendiente == false){
+                
+                       /*if (!System.IO.File.Exists("AH.dll"))
+                        {
+                        int num1 = (int) MessageBox.Show("No se ha podido encontrar el antihack!", "Error", MessageBoxButton.OK, MessageBoxImage.Hand);
+                        }*/
+                        if (!System.IO.File.Exists("WinterAO Resurrection.exe")) {
+                            int num2 = (int)MessageBox.Show("No se ha encontrado el ejecutable de WinterAO Resurrection!", "Error", MessageBoxButton.OK, MessageBoxImage.Hand);
+                        } else if (new Process() {
+                            StartInfo = new ProcessStartInfo("WinterAO Resurrection.exe")
+                        }.Start()) ;
+
+                    }else{ //Las hay, comenzamos el proceso de actualizacion
+                        this.Actualizando = true;
+                        this.Actualizar();
+                    }
+                }
+            }
+
+            /**
+             * Boton de minimizar
+             */
+            private void btnMini_Click(object sender, RoutedEventArgs e){
+                this.WindowState = WindowState.Minimized;
+            }
   }
 }
